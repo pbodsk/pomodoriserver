e7933a7a198ff790540b3b762742dbd1c0b5b6cb
Peter Bødskov <peter.boedskov@jyskebank.dk>
2013-12-05 08:42:50 +0100

    added model and started work on POST method

--- app.rb
+++ app.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './config/environments'
+require './models/pomodoriSession'
+require 'json'
 
 
 get '/' do 
   "Hello World"
+end
+
+=begin
+before do
+  if request.request_method == "POST"
+    body_parameters = request.body.read
+    params.merge!(JSON.parse(body_parameters))
+  end
+end
+=end
+
+post '/update' do
+  content_type :json
+  @input_data = JSON.parse(request.body.read)
+  @pomodori_session = PomodoriSession.new(@input_data)
+  if(@pomodori_session.save)
+    "saved!"
+  else
+    "not saved"
+  end
 end
---
+++ models/pomodoriSession.rb
@@ -0,0 +1,3 @@
+class PomodoriSession < ActiveRecord::Base
+  
+end
