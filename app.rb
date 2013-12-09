f75ca9780b1785305d4879da574a9b93700115eb
Peter Bødskov <peter.boedskov@jyskebank.dk>
2013-12-09 22:33:34 +0100

    working continued

--- app.rb
+++ app.rb
@@ -11,10 +11,26 @@ end
 post '/update' do
   content_type :json
   @input_data = JSON.parse(request.body.read)
-  @pomodori_session = Session.new(@input_data)
-  if(@pomodori_session.save)
-    "saved!"
+  username = @input_data['username']
+  group = @input_data['group']
+  session = Session.find_by username: username, group: group
+  if(session)
+    session.remainingtime = @input_data['remainingtime']
+    session.touch
   else
-    "not saved"
+    session = Session.new(@input_data)
   end
+  save_session(session)
+end
+
+private
+def save_session(session)
+  session.save
+  generate_return_json(session)
+end
+
+def generate_return_json(session)
+  Session.where(updated_at: 2.minutes.ago, group: session.group).destroy_all
+  sessions = Session.find_by group: session.group
+  sessions.to_json  
 end
