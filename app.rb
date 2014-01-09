cd0f9c4231decb10a712d381a8b2230f11701833
Peter Bødskov <peter.boedskov@jyskebank.dk>
2014-01-09 20:55:14 +0100

    fixed #1

--- app.rb
+++ app.rb
@@ -13,9 +13,8 @@ post '/update' do
   #to be used with cURL calls
 #  @input_data = JSON.parse(request.body.read)
   @input_data = params
-  username = @input_data['username']
-  group = @input_data['group']
-  session = Session.find_by username: username, group: group
+  read_params
+  session = find_session
   if(session)
     session.remainingtime = @input_data['remainingtime']
     session.status = @input_data['status']
@@ -26,7 +25,29 @@ post '/update' do
   save_session(session)
 end
 
+post '/remove' do
+  content_type :json
+  #to be used with cURL calls
+#  @input_data = JSON.parse(request.body.read) 
+  @input_data = params
+  read_params
+  session = find_session
+  if session
+    session.destroy
+    session.to_json
+  end
+end
+
 private
+def read_params
+  @username = @input_data['username']
+  @group = @input_data['group'] 
+end
+
+def find_session
+  Session.find_by username: @username, group: @group
+end
+
 def save_session(session)
   session.save
   generate_return_json(session)
